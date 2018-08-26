fed99402b61505233f2b4f038278692399b9f3ef
markdyousef <markdyousef@gmail.com>
2018-08-26 16:01:22 +0200

    simple apollo express setup

--- src/index.js
+++ src/index.js
@@ -1,1 +1,39 @@
 import "dotenv/config";
+import express from "express";
+import cors from "cors";
+import { ApolloServer, gql } from "apollo-server-express";
+
+const { PORT } = process.env;
+
+const app = express();
+app.use(cors());
+
+const schema = gql`
+  type Query {
+    me: User
+  }
+
+  type User {
+    username: String!
+  }
+`;
+const resolvers = {
+  Query: {
+    me: () => {
+      return {
+        username: "Mark Yousef"
+      };
+    }
+  }
+};
+
+const server = new ApolloServer({
+  typeDefs: schema,
+  resolvers
+});
+
+server.applyMiddleware({ app, path: "/graphql" });
+
+app.listen({ port: PORT }, () => {
+  console.log(`Apollo Server on port: ${PORT}`);
+});
